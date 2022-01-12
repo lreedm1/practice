123b5a920281e6a7d257d69a8ecef61f13b068e5
lreedm1 <lreedm1@gmail.com>
2022-01-11 20:53:44 -0600

    Starting tkinter program to use with gpt-3 Literally just got window working, the rest will follow

---
+++ main.py
@@ -0,0 +1,51 @@
+# credit to source - https://dev.to/insidiousthedev/make-a-simple-text-editor-using-python-31bd
+
+from tkinter import *
+from tkinter.filedialog import *
+from tkinter.messagebox import *
+from tkinter.messagebox import *
+from tkinter.font import Font
+from tkinter.scrolledtext import *
+import file_menu
+'''
+import edit_menu
+import fomrat_menu
+import help_menu
+'''
+
+#create the main window
+root = Tk()
+root.title("Text Editor")
+
+#define the size of the window
+root.geometry("800x600")
+root.minsize(300,300)
+
+
+#define the scrollbar behavior
+scrollbar = Scrollbar(root)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+
+#define the text widget
+text = Text(root, yscrollcommand=scrollbar.set)
+text.pack(fill=BOTH, expand=1)
+scrollbar.config(command=text.yview)
+
+#define the font
+font = Font(family="Arial", size=14)
+text.config(font=font)
+
+
+#create the menu bar
+menubar = Menu(root)
+
+
+#add menus to the menu bar
+file_menu.main(root, text, menubar)
+#edit_menu.main(root, text, menubar)
+#format_menu.main(root, text, menubar)
+#help_menu.main(root, text, menubar)
+
+#run the main window
+root.mainloop()
